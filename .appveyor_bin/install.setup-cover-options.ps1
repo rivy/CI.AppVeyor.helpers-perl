3ee436f1ae7c104dd4a06eccf889b81a9fde672a
Roy Ivy III <rivy.dev@gmail.com>
2018-05-06 01:19:14 -0500

    multiple disparate improvements

--- .appveyor_bin/install.setup-cover-options.ps1
+++ .appveyor_bin/install.setup-cover-options.ps1
@@ -5,11 +5,10 @@ if (! $env:DEVEL_COVER_OPTIONS) {
 	$options = @()
 	if ( $env:DIST_TOOLING -ieq "build" ) { $options += '-ignore,^_build/' }
 	if ( ! $env:DIST_TOOLING ) {
-		if ((get-item 't' -ea ignore) -is [IO.DirectoryInfo]) { $options += $options += '-ignore,^t/' }
-		if ((get-item 'xt' -ea ignore) -is [IO.DirectoryInfo]) { $options += $options += '-ignore,^xt/' }
+		if ((get-item 't' -ea ignore) -is [IO.DirectoryInfo]) { $options += '-ignore,^t/' }
+		if ((get-item 'xt' -ea ignore) -is [IO.DirectoryInfo]) { $options += '-ignore,^xt/' }
 		}
     $env:DEVEL_COVER_OPTIONS = $options -join ","
-    # write-host "env:COVERAGE = $env:COVERAGE"
 }
 
 set-PSdebug -off
