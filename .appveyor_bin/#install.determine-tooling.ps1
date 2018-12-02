d2f55f1583dd3b4009c77b946328db60833c6474
Roy Ivy III <rivy.dev@gmail.com>
2018-12-02 00:23:56 -0600

    wip ~ move determine-tooling into sub-script

---
+++ .appveyor_bin/#install.determine-tooling.ps1
@@ -0,0 +1,9 @@
+# determine repository tooling
+if ( $env:CI_DEBUG ) { set-PSdebug -trace 1 }
+
+if ( ! $env:DIST_TOOLING -and (Test-Path "build.PL") ) { $env:DIST_TOOLING = "build" }
+if ( ! $env:DIST_TOOLING -and (Test-Path "Makefile.PL") ) { $env:DIST_TOOLING = "make" }
+
+if ( $env:DIST_TOOLING -ieq "build" -and $($_=$(perl -MModule::Build -e "print Module::Build->VERSION()).Replace("_",""); $_ -lt 0.421) ) { write-host -f yellow "WARN using buggy early version of Module::Build ($_); consider requiring later version" }
+
+set-PSdebug -off
