0955d07aef484039d2d93d90afa9e637208d7c92
Roy Ivy III <rivy.dev@gmail.com>
2018-12-02 09:07:16 -0600

    wip ~ F! (API) change to 'determine-tooling'

--- .appveyor_bin/#install.determine-tooling.ps1
+++ .appveyor_bin/#install.determine-tooling.ps1
@@ -4,6 +4,6 @@ if ( $env:CI_DEBUG ) { set-PSdebug -trace 1 }
 if ( ! $env:DIST_TOOLING -and (Test-Path "build.PL") ) { $env:DIST_TOOLING = "build" }
 if ( ! $env:DIST_TOOLING -and (Test-Path "Makefile.PL") ) { $env:DIST_TOOLING = "make" }
 
-if ( $env:DIST_TOOLING -ieq "build" -and $($_=$(perl -MModule::Build -e "print Module::Build->VERSION()).Replace("_",""); $_ -lt 0.421) ) { write-host -f yellow "WARN using buggy early version of Module::Build ($_); consider requiring later version" }
+if ( $env:DIST_TOOLING -ieq "build" -and $($_=$(perl -MModule::Build -e "print Module::Build->VERSION()").Replace("_",""); $_ -lt 0.421) ) { write-host "WARN: using buggy early version of Module::Build ($_); consider requiring later version" -f yellow }
 
 set-PSdebug -off
