4b6ef1ffdced1ac99f27b04f9a80043e74cb56c2
Roy Ivy III <rivy.dev@gmail.com>
2018-12-02 13:09:59 -0600

    F! transition to PS! only API

--- .appveyor_bin/#install.setup-coverage.ps1
+++ .appveyor_bin/#install.setup-coverage.ps1
@@ -8,18 +8,18 @@ if ( ! $env:CI_HELPERS ) { $env:CI_HELPERS = [IO.Path]::GetFullPath(".") }
 if (-not $env:CI_SKIP -and -not $env:OS_unsupported -and $env:COVERAGE) {
     # install coverage support
     & "${env:CI_HELPERS}\cpanm-mods_only.BAT" @( '--no-interactive', '--no-man-pages', '--notest', '--quiet', '--skip-satisfied', 'Devel::Cover' )
-    if ( $LASTEXITCODE -eq 0 ) { $exit_val = $LASTEXITCODE }
+    if ( $LASTEXITCODE -ne 0 ) { $exit_val = $LASTEXITCODE }
     ($env:COVERAGE).split() | foreach {
         # echo "\"${env:CI_HELPERS}\cpanm-mods_only.BAT\" --no-interactive --no-man-pages --notest --quiet --skip-satisfied Devel::Cover::Report::$_"
         if ( $_ -ieq "Coveralls" ) {
             ## override for flawed default "Coveralls"; use patched version from personal github repo
             # "${env:CI_HELPERS}\cpanm-mods_only.BAT" --no-interactive --no-man-pages --notest --quiet --skip-satisfied https://github.com/rivy/perl.Devel-Cover-Report-Coveralls.git
             & "${env:CI_HELPERS}\cpanm-mods_only.BAT" @( '--no-interactive', '--no-man-pages', '--notest', '--quiet', '--skip-satisfied', 'https://github.com/rivy/perl.Devel-Cover-Report-Coveralls.git' )
-            if ( $LASTEXITCODE -eq 0 ) { $exit_val = $LASTEXITCODE }
+            if ( $LASTEXITCODE -ne 0 ) { $exit_val = $LASTEXITCODE }
         } else {
             # "${env:CI_HELPERS}\cpanm-mods_only.BAT" --no-interactive --no-man-pages --notest --quiet --skip-satisfied Devel::Cover::Report::$_
             & "${env:CI_HELPERS}\cpanm-mods_only.BAT" @( '--no-interactive', '--no-man-pages', '--notest', '--quiet', '--skip-satisfied', "Devel::Cover::Report::$_" )
-            if ( $LASTEXITCODE -eq 0 ) { $exit_val = $LASTEXITCODE }
+            if ( $LASTEXITCODE -ne 0 ) { $exit_val = $LASTEXITCODE }
         }
     }
 }
